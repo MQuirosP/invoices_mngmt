7e388dd66e62923a5b14cbe2b4473f4d9e735a18
Mario Quirós <mquirosp78@gmail.com>
2025-07-07 16:03:32 -0600

    feat(Warranty module): Schema, Service, Controller & Service created

---
+++ backend/src/modules/warranties/routes/warranty.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { create, findByInvoice, update, remove } from "../controller/warranty.controller";
+import { authenticate } from "../../auth/middleware/auth.middleware";
+
+const router = Router();
+
+router.post("/", authenticate, create);
+router.get("/:invoiceId", authenticate, findByInvoice);
+router.put("/:invoiceId", authenticate, update);
+router.delete("/:invoiceId", authenticate, remove);
+
+export default router;
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import authRouter from "../modules/auth/routes/auth.routes";
 import invoiceRouter from "../modules/invoices/routes/invoice.routes";
+import warrantyRouter from "../modules/warranties/routes/warranty.routes"
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.get("/", (req, res) => {
 // Modulo de autenticación
 router.use("/auth", authRouter);
 router.use("/invoices", invoiceRouter);
+router.use("/warranties", warrantyRouter)
 
 export default router;
