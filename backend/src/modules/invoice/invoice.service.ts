c2af66368ef68b6ff997e7c6a797ae4cce154a48
Mario Quirós <mquirosp78@gmail.com>
2025-07-11 14:15:27 -0600

    modified:   backend/src/modules/invoice/invoice.service.ts

--- backend/src/modules/invoice/invoice.service.ts
+++ backend/src/modules/invoice/invoice.service.ts
@@ -2,9 +2,8 @@ import { prisma } from "@/config/prisma";
 import { AppError } from "@/shared/utils/AppError.utils";
 import { CreateInvoiceInput } from "@/modules/invoice";
 import axios from "axios";
-// import { OCRService } from "@/shared/services/ocr.service";
 import { ExtractedMetadata } from "@/shared/utils/extractMetadata.utils";
-import { OCRService } from "../../shared/services/ocr.service";
+import { OCRService } from "@/shared/services/ocr.service";
 
 export const createInvoice = async (
   data: CreateInvoiceInput,
